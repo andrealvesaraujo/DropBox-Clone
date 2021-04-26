e91df7de294621dd9e6ad7b7b640f0cebc8eac09
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-04-26 16:22:09 -0300

    D13 - Criando e disparando eventos

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -3,6 +3,9 @@ class DropBoxController {
     // cd app 
     // npm start
     constructor(){
+
+        this.onselectionchange = new Event('selectionchange'); 
+
         this.btnSendFileEl = document.querySelector("#btn-send-file");
         this.inputFilesEl = document.querySelector("#files");
         this.snackModalEl = document.querySelector("#react-snackbar-root");     
@@ -33,6 +36,11 @@ class DropBoxController {
 
     initEvents(){
 
+        this.listFilesEl.addEventListener('selectionchange', e=>{
+
+            console.log('selectionchange');
+
+        });
 
         this.btnSendFileEl.addEventListener('click', event=>{
 
@@ -373,6 +381,8 @@ class DropBoxController {
     initEventsLi(li) {
 
         li.addEventListener('click', e=>{
+
+            this.listFilesEl.dispatchEvent(this.onselectionchange);
 
             if(e.shiftKey) {
 
