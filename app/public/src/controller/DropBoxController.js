f7dfe322c1892dfb0b182585d171cfc0fb60f899
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-04-27 23:32:24 -0300

    D23 - Abrindo o arquivo

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -707,7 +707,7 @@ class DropBoxController {
                     this.openFolder();
                     break;
                 default:
-                    window.open('/file?path='+file.path);
+                    window.open(file.path);
 
             }
 
